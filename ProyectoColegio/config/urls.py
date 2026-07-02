18473704dc728b79d6de68917dc5696ccf34e112
JuanGuillermoPerezVega <jperezv2@soy.sena.edu.co>
2026-07-02 16:19:19 -0500

    prueba17

--- ProyectoColegio/config/urls.py
+++ ProyectoColegio/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='login:login', permanent=False)),
     path('admin/', admin.site.urls),
     path('colegio/', include(('app.urls', 'app'), namespace='app')),
     path('', include(('login.urls', 'login'), namespace='login')),
